d04f252624894080b95e43ec2a716196dace6054
pranavpowar <powarpranav@icloud.com>
2020-04-06 00:27:12 +0530

    couple of changes related to pipes , strong types, style encapsulation etc

--- DunderMifflinProductsManager/ClientApp/src/app/products/product-list.component.ts
+++ DunderMifflinProductsManager/ClientApp/src/app/products/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { IProduct } from "./product";
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
   pageTitle: string = "Product List";
@@ -10,26 +12,26 @@ export class ProductListComponent {
   imageMargin: number = 2;
   showImage: boolean = false;
   listFilter: string = "A4 Paper";
-  products: any[] = [
+  products: IProduct[] = [
     {
-      "productID": 2,
+      "productId": 2,
       "productName": "A4 Paper",
       "productCode": "P-RSA4",
       "releaseDate": "January 20, 2003",
       "description": "Set of 300 renewed A4 size papers",
       "price": 32.99,
       "starRating": 4.2,
-      "imageURL": "https://sc02.alicdn.com/kf/UTB8qDqZtYPJXKJkSafSq6yqUXXaj.jpg_350x350.jpg"
+      "imageUrl": "https://sc02.alicdn.com/kf/UTB8qDqZtYPJXKJkSafSq6yqUXXaj.jpg_350x350.jpg"
     },
     {
-      "productID": 3,
+      "productId": 3,
       "productName": "Sticky Notes",
       "productCode": "P-SN20",
       "releaseDate": "April 5, 2004",
       "description": "Bundle of 40 Sticky Notes",
       "price": 4.9,
       "starRating": 4.8,
-      "imageURL": "https://5.imimg.com/data5/PX/IX/MY-38990110/post-it-sticky-notes-500x500.jpg"
+      "imageUrl": "https://5.imimg.com/data5/PX/IX/MY-38990110/post-it-sticky-notes-500x500.jpg"
     }
   ];
 
